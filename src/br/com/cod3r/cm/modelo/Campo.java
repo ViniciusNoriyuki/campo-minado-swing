9e9f321899bebbd99142068f5872bd2a8fcde156
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-08-16 11:08:54 -0300

    Jogo Campo Minado + Swing

---
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -0,0 +1,145 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+	private List<CampoObservador> observadores = new ArrayList<>();
+	
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	public void registrarObservador(CampoObservador observador) {
+		observadores.add(observador);
+	}
+	
+	private void notificarObservadores(CampoEvento evento) {
+		observadores.stream()
+			.forEach(o -> o.eventoOcorreu(this, evento));
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		// Se o campo candidato estiver na linha e na coluna diferente do campo, significa que está na diagonal
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		/*
+		 * Se a diferença absoluta entre a linha e coluna do campo com o vizinho for:
+		 * 1 = vizinho está dos lados ou em cima ou embaixo;
+		 * 2 = vizinho está na diagonal.
+		 */
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if(deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if(deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;	// Alternar a marcação
+			
+			if(marcado) {
+				notificarObservadores(CampoEvento.MARCAR);
+			} else {
+				notificarObservadores(CampoEvento.DESMARCAR);
+			}
+		}
+	}
+	
+	public boolean abrir() {
+		if(!aberto && !marcado) {
+			if(minado) {
+				notificarObservadores(CampoEvento.EXPLODIR);
+				return true;
+			}
+			
+			setAberto(true);
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());	// Abre todos os vizinhos desde que a vizinhança esteja segura
+			}
+			
+			return true;
+		} else {
+			return false;			
+		}
+	}
+	
+	public boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);	// Verifica se todos os vizinhos não estão minados
+	}
+	
+	void minar() {
+		minado = true;  
+	}
+	
+	public boolean isMinado() {
+		return minado;
+	}
+	
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	void setAberto(boolean aberto) {
+		this.aberto = aberto;
+		
+		if(aberto) {
+			notificarObservadores(CampoEvento.ABRIR);
+		}
+	}
+
+	public boolean isAberto() {
+		return aberto;
+	}
+
+	public boolean isFechado() {
+		return !aberto;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+	
+	public int minasNaVizinhanca() {
+		return (int) vizinhos.stream().filter(v -> v.minado).count();
+	}
+	
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+		notificarObservadores(CampoEvento.REINICIAR);
+	}
+}
